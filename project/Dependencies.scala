bf7845470d7e516bbef433b92ed2789b0e23c058
Scala Steward <2992938+DavidGregory084@users.noreply.github.com>
2023-08-01 10:51:39 +0100

    Update logback-classic to 1.2.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
 
   val playJsonVersion = "2.9.2"
 
-  val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
+  val logback = "ch.qos.logback" % "logback-classic" % "1.2.12"
 
   val specs2Version = "4.10.6"
   val specs2Deps = Seq(
